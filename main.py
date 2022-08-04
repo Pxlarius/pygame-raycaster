205ff743b5d3cc99e83b42fe9844185d3430992a
lyezene <61851524+lyezene@users.noreply.github.com>
2022-08-04 13:05:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,49 @@ from pygame.locals import *
 import math as m
 
 #set screen dimensions and fps cap
+fps = 30
+size = width, height =  400, 400
+windowSize = pygame.display.set_mode((size))
+white = (255, 255, 255)
 
 #construct environmet as matrix
+environment = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+]
 
 #initialize fov, x/ypos and rotation variables
+fov = 60
+xPos = 1
+yPos = 1
+rot = 0
 
 #initialize sensitivity and movement speed variables
+sensitivity = 1
+movementSpeed = 1
 
 #create movement directions
 
 #initialize precision variable (how frequently objects are checked for in a given ray)
+precision = 0.02
 
 #fps cap and movement
+clock = pygame.time.Clock()
 
 #raycasting engine
+running = True
+while running:
+    pygame.init()
+    clock.tick(fps)
+    pygame.display.set_caption('FPS - ' + str(round(clock.get_fps())))
+    for event in pygame.event.get():
+        #pygame.draw.rect(windowSize, white, (200, 150, 100, 50))
+        pygame.display.flip()
+        if event.type == pygame.QUIT:
+            sys.exit()
     #loop that applies to all values within the players fov
         #define player position and rotation
         #calculates side length of fov via trig
