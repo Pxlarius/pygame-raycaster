63d355134903d146dd56a1cb7c8c6bb64deef1f0
lyezene <61851524+lyezene@users.noreply.github.com>
2022-08-05 10:01:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 from pygame.locals import *
 import math as m
 
-#set screen dimensions and fps cap
+# set screen dimensions and fps cap
 fps = 30
-size = width, height =  400, 400
-windowSize = pygame.display.set_mode((size))
+size = width, height = 400, 400
+windowSize = pygame.display.set_mode(size)
 white = (255, 255, 255)
 
-#construct environmet as matrix
+# construct environment as matrix
 environment = [
     [1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
@@ -17,39 +17,42 @@ environment = [
     [1, 1, 1, 1, 1],
 ]
 
-#initialize fov, x/ypos and rotation variables
+# initialize fov, x/ypos and rotation variables
 fov = 60
 xPos = 1
 yPos = 1
 rot = 0
 
-#initialize sensitivity and movement speed variables
+# initialize sensitivity and movement speed variables
 sensitivity = 1
 movementSpeed = 1
 
-#create movement directions
+# create movement directions
 
-#initialize precision variable (how frequently objects are checked for in a given ray)
+# initialize precision variable (how frequently objects are checked for in a given ray)
 precision = 0.02
 
-#fps cap and movement
+# fps cap and movement
 clock = pygame.time.Clock()
 
-#raycasting engine
+# raycasting engine
 running = True
 while running:
     pygame.init()
     clock.tick(fps)
     pygame.display.set_caption('FPS - ' + str(round(clock.get_fps())))
     for event in pygame.event.get():
-        #pygame.draw.rect(windowSize, white, (200, 150, 100, 50))
+        # pygame.draw.rect(windowSize, white, (200, 150, 100, 50))
         pygame.display.flip()
         if event.type == pygame.QUIT:
-            sys.exit()
-    #loop that applies to all values within the players fov
-        #define player position and rotation
-        #calculates side length of fov via trig
-        #loop that updates fov constantly
-            #renders environment if 1 is detected in matrix
-        #draws environment with pygame.draw.line
-        #height of object should correlate to distance form object (the further away an object is the smaller it will be)
+            exit()
+    # loop that applies to all values within the players fov
+    for i in range(fov):
+        print('i')
+        # define player position and rotation
+
+        # calculates side length of fov via trig
+        # loop that updates fov constantly
+        # renders environment if 1 is detected in matrix
+    # draws environment with pygame.draw.line
+        # height of object should correlate to distance form object
