8b1321eeda896e5c8c5ca1cdb55198d75299d963
Pxlarius <110715299+Pxlarius@users.noreply.github.com>
2022-08-06 17:23:21 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 from pygame.locals import *
 import math as m
@@ -19,8 +21,8 @@ environment = [
 
 # initialize fov, x/ypos and rotation variables
 fov = 60
-xPos = 1
-yPos = 1
+xPos = 1.5
+yPos = 1.5
 rot = 0
 
 # initialize sensitivity and movement speed variables
@@ -42,22 +44,33 @@ while running:
     clock.tick(fps)
     pygame.display.set_caption('FPS - ' + str(round(clock.get_fps())))
     for event in pygame.event.get():
+
         # pygame.draw.rect(windowSize, white, (200, 150, 100, 50))
         pygame.display.flip()
         if event.type == pygame.QUIT:
             exit()
     # loop that applies to all values within the players fov
     for i in range(fov+1):
-        # newRotation = old rotation + change
-        # i - fov/2 = fov angle
+        # New Rotation = old Rotation + Change
+        # Radians = Distance of View
+        # i - fov/2 = Field of View
+        # Radian Formula = m.pi/180 * VALUE
+        # 2*m.pi Radians in a full circle
         newRot = rot + m.radians(i - fov/2)
-        # define player position
+        dx = xPos % 1
+        dy = 1 - (yPos % 1)
+        #Distance to reach grid point
+        dxlength = dy/m.tan(newRot + 1)
+        dylength = dx*m.tan(newRot + 1)
+        xstepUnit = -1/m.tan(newRot + 1)
+        ystepUnit = 1/m.tan(newRot + 1)
+        xIntercept = xPos + dx + dy / m.tan(newRot)
+        yIntercept = yPos + dx + dx / m.tan(newRot)
+        print(ystepUnit)
+        # define player position and rotation
         vertAx, vertAy = xPos, yPos
-        # define contact between ray and object
-        vertBx, vertBy = vertAx + m.radians(i - fov/2), vertAy + m.radians(i - fov/2)
         # calculates side length of fov via trig
-        # lineC = (lineA)*(lineA)+(lineB)*(lineB) - 2(lineA*lineB)*m.cos(fov/2)
         # loop that updates fov constantly
         # renders environment if 1 is detected in matrix
-    # draws environment with pygame.draw.line
+        # draws environment with pygame.draw.line
         # height of object should correlate to distance form object
